e026d0b666e31a297f16fb470d06513479cb3e03
Stanislav Havriushchenko <shavriushchenko@gmail.com>
2019-07-17 18:16:51 +0300

    Initial commit.

---
+++ Test_js/main.js
@@ -0,0 +1,175 @@
+// let a=6;
+// let b=3;
+// let c=a-b;
+// let d=c-7;
+// let result=c-d;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a/b);
+// console.log(a*b);
+// console.log(c);
+// console.log(result);
+
+// let str1="Hello ";
+// let str2="World";
+
+// let str3=str1+str2;
+// console.log(str3);
+
+// let num1="12345";
+// console.log(num1[0]*num1[1]*num1[2]*num1[3]*num1[4]);
+
+// let arr=["Hello ","World","!"];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// let str4=arr[0]+arr[1]+arr[2];
+// console.log(str4);
+// let arr1=["a","b","c","d"];
+// console.log(arr1[0]+arr1[1]);
+
+// let arr2=[1,2,3,4];
+// result2=arr2[0]*arr2[1]+arr2[2]*arr2[3];
+// console.log(result2);
+
+// let obj={a:1,b:2,c:3};
+
+
+// var arr3=[ [1, 2, 3], [4, 5, 6], [7,8,9] ];
+
+// console.log(arr3[2][2]);
+
+// if (a>0) {
+// 	console.log("OK!");
+// }else{
+// 	console.log("NOK!");
+// };
+
+// if(a>=0 && a<15){
+// 	console.log("First Quarter");
+// };
+// if(a>=15 && a<30){
+// 	console.log("Second Quarter");
+// };
+// if(a>=30 && a<44){
+// 	console.log("Third Quarter");
+// };
+// if(a>=45 && a<59){
+// 	console.log("Fourth Quarter");
+// };
+  
+// var lang="en";
+
+// if(lang=="ru"){
+// 	var days=["пн","вт","ср","чт","пт"];
+// }
+// if(lang=="en"){
+// 	var days=["mn","th","wd","th","fr"];
+// }
+
+// var lang="by";
+// switch(lang){
+// 	case "by":
+// 	var days=["пн","вт","ср","чт","пт"];
+// 	break;
+// 	case "su":
+// 	var days=["mn","th","wd","th","fr"];
+// 	break;
+// }
+
+// // console.log(days);
+
+// // let test=1;
+// // if(test!==false){
+// // 	alert("Ok!");
+// // }
+
+
+
+// // for (var i =1;i<=50; i++) {
+	
+// // 	console.log(i);
+// // }
+
+// // var i=1
+// // while(i<=50){
+// //    console.log(i);
+// //    i++
+// // }
+// var result3=1;
+// let arr10=[1,2,3,4,5,6];
+// for(var i=0;i<arr10.length;i++){	
+// 	result3=result3*arr10[i];	
+// }
+
+// var result4=0;
+// let arr10=[1,2,3,4,5,6];
+// for(var i=0;i<arr10.length;i++){
+// 	result4=result4+arr10[i];		
+// }
+
+
+
+// console.log(result4);
+
+// var result = 1;
+// var arr = [2, 3, 4, 5];
+// for (var i = 0; i < arr.length; i++) {
+// 	result *= arr[i];
+// }
+// alert(result);
+
+// var a = 10;
+// var b = 3;
+// alert(a % b);
+
+// console.log(Math.pow(3,3));
+// let q=(Math.sqrt(379,2));
+// document.getElementById("click-left").onclick=moveLeft;
+
+    
+// function moveLeft(){
+//    var slider=document.getElementById("slider");
+//    left=left-500;
+//    if(left<-2000){
+//    	left=0;
+//    }
+//    slider.style.left=left+"px";
+// }
+ 
+// let ex=document.getElementsByClassName("ex");
+// for(let i=0;i<ex.length;i++){
+// 	ex[i].style.color="red";	
+// }
+
+// document.getElementById("click-left").onclick=color;
+
+// function color(){	 
+// let ex=document.getElementsByClassName("ex");
+// for(let i=0;i<ex.length;i++){
+// 	ex[i].style.color="green";	
+// }
+// }
+
+// let str="I learn PHP";
+// let newstr=str.replace("PHP","Javascript");
+// console.log(newstr);
+
+// let str1="1 2 3 4 5 6 7 8 9";
+// let arr=str1.split("");
+// console.log(arr);
+// let result=arr.reverse();
+// let str2=result.join("");
+// console.log(str2);
+
+// function hasElem(arr){
+// 	for(var i=0;i<arr.length;i++){
+// 		if(arr[i]=="r"){
+// 			return true;
+// 		}
+// 	}
+// 	return false;
+// }
+
+// var arr=["a","b","c","d"];
+// console.log(hasElem(arr));
